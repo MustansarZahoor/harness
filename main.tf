c3b414a381dc50229fb067c9d3ea8d57faa698fd
Pawar, Mithika [OT-TECH NE] <mp90403@citi.com>
2024-02-16 15:01:36 +0500

    first commit

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1" # Specify your desired region here
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "tesstiacfeb16"
+
+  tags = {
+    Name        = "MyBucket"
+    Environment = "Production"
+    CreatedBy   = "Terraform"
+  }
+}
